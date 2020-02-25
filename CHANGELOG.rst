bc80e41cfa68ebb84aee9165b67d52429834d258
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-02-25 15:37:16 +0100

    Bump version: 0.1.0 → 0.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,11 @@
 Changelog
 =========
 
+0.1.1 (2020-02-25)
+------------------
+
+* Fix bumpversion config causing invalid 'version=0.1.0' version.
+
 0.1.0 (2020-02-25)
 ------------------
 
--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "nameko-chassis"
 year = "2020"
 author = "Emplocity"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.1.0"
+version = release = "0.1.1"
 
 pygments_style = "trac"
 templates_path = ["."]
