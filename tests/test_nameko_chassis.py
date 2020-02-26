03a45da69e8b1e3ffecb048f759bae6b1106c51a
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-02-26 13:39:15 +0100

    Add meaningful test for say_hello.

--- tests/test_nameko_chassis.py
+++ tests/test_nameko_chassis.py
@@ -1,6 +1,12 @@
+from nameko.testing.services import worker_factory
+
 from nameko_chassis.service import Service
 
 
-def test_service():
-    # TODO
-    _ = Service()
+class MyService(Service):
+    name = "my_service"
+
+
+def test_say_hello():
+    service = worker_factory(MyService)
+    assert service.say_hello() == "Hello from my_service!"
