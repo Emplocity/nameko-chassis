2e4781524fda78e9a2068e6bb9e1caed2da03f74
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-07-29 20:53:27 +0200

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="nameko-chassis",
-    version="0.5.0",
+    version="0.6.0",
     license="Apache-2.0",
     description="nameko-chassis provides an opinionated base class for building resilient, observable microservices. ",
     long_description=long_description,
