365493c8de53c4ddd82f02fe6b86a92693530ede
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-08-23 12:32:49 +0200

    Switch tests to integration style.
    The tests now require RabbitMQ to be up running and use
    container_factory to setup a real service container with real
    dependencies.

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,35 +1,53 @@
 import logging
 
-from nameko.testing.services import worker_factory
-from nameko_prometheus.dependencies import MetricsServer
+import pytest
+from nameko import config
+from nameko.testing.services import entrypoint_hook
+from nameko_prometheus.utils import reset_prometheus_registry
 from werkzeug.test import EnvironBuilder
 
 from nameko_chassis.service import Service
+
+
+@pytest.fixture(autouse=True)
+def rabbit_config():
+    with config.patch({"AMQP_URI": "pyamqp://localhost:5672"}):
+        yield
+
+
+@pytest.fixture(autouse=True)
+def reset_registry():
+    reset_prometheus_registry("my_service")
 
 
 class MyService(Service):
     name = "my_service"
 
 
-def test_say_hello():
-    service = worker_factory(MyService)
-    assert service.say_hello() == "Hello from my_service!"
+def test_say_hello(container_factory):
+    container = container_factory(MyService)
+    container.start()
+    with entrypoint_hook(container, "say_hello") as hook:
+        assert hook() == "Hello from my_service!"
 
 
-def test_serve_metrics():
+def test_serve_metrics(container_factory):
     request = EnvironBuilder(
         method="GET", path="/metrics", headers={"Accept": "text/html"}
     ).get_request()
-    # replace metrics mock with a real dependency from nameko-prometheus
-    service = worker_factory(MyService, metrics=MetricsServer())
-    response = service.serve_metrics(request)
-    assert response.status_code == 200
+    container = container_factory(MyService)
+    container.start()
+    with entrypoint_hook(container, "serve_metrics") as hook:
+        response = hook(request)
+        assert response.status_code == 200
 
 
-def test_set_log_level():
-    service = worker_factory(MyService)
+def test_set_log_level(container_factory):
+    container = container_factory(MyService)
+    container.start()
     logger = logging.getLogger("foo.bar")
     logger.setLevel(logging.ERROR)
-    service.set_log_level("foo.bar", logging.DEBUG)
+    with entrypoint_hook(container, "set_log_level") as hook:
+        hook("foo.bar", logging.DEBUG)
     logger = logging.getLogger("foo.bar")
     assert logger.level == logging.DEBUG
